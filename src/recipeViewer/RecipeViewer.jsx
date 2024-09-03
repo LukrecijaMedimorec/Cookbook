f378a773e02e88a6c8e1997663911ffef25fe6ab
LukrecijaMedimorec <medimoreclukrecija@gmail.com>
2024-09-03 23:02:20 +0200

    Add delete

--- src/recipeViewer/RecipeViewer.jsx
+++ src/recipeViewer/RecipeViewer.jsx
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './recipe_viewer.css';
 import RecipeCard from "../recipeCard/RecipeCard";
 import defaultImage from "../assets/doughnuts.jpg";
 import SearchBar from "../searchBar/SearchBar";
 import Button from "../button/Button";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
 
 const RecipeViewer = () => {
-    const [recipes, setRecipes] = useState([]); // State to hold the list of recipes
+    const [recipes, setRecipes] = useState([]);
     const navigate = useNavigate();
 
-    // Fetch recipes from API when component mounts
     useEffect(() => {
         const fetchRecipes = async () => {
             try {
-                const response = await axios.get('http://localhost:8080/1/recipes/getAll'); // Replace with your API endpoint
+                const response = await axios.get('http://localhost:8080/1/recipes/getAll');
                 setRecipes(response.data);
             } catch (error) {
                 console.error('Error fetching recipes:', error);
@@ -29,22 +28,36 @@ const RecipeViewer = () => {
         navigate('/upload');
     };
 
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(`http://localhost:8080/1/recipes/${id}`);
+            setRecipes(recipes.filter(recipe => recipe.id !== id));
+        } catch (error) {
+            console.error('Error deleting recipe:', error);
+        }
+    };
+
     return (
         <div className="viewer">
             <SearchBar />
             <Button onClick={handleRecipeRedirect} textColor="black" color="white" fontSize="20px">New recipe</Button>
-            <div className="recipe-cards-container">
-                {recipes.map((recipe) => (
+            {recipes.map(recipe => (
+                <div key={recipe.id} className="recipe-card-container">
                     <RecipeCard
-                        key={recipe.id}
                         title={recipe.title}
                         author={recipe.author}
-                        description={recipe.content}
+                        description={recipe.content} // Map content to description
                         tags={recipe.tags}
                         image={defaultImage}
                     />
-                ))}
-            </div>
+                    <button
+                        className="delete-button"
+                        onClick={() => handleDelete(recipe.id)}
+                    >
+                        Delete
+                    </button>
+                </div>
+            ))}
         </div>
     );
 };
